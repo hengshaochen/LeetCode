19686945e17e9ac62201a61c5d4f9743eb5813b6
hengshaochen <henrychen0702@gmail.com>
2017-08-05 14:08:44 +0800

    binary_search

---
+++ binary_search.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return an integer
+     */
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) { return -1; }
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (target == nums[mid]) {
+                end = mid; // 若要找first position
+                // start = mid; 若要找last position
+            } else if (target > nums[mid]) {
+                start = mid;
+            } else if (target < nums[mid]) {
+                end = mid;
+            }
+        }
+        
+        if (nums[end] == target) { return end; }
+        if (nums[start] == target) { return start; }
+        return -1;
+    }
+}
