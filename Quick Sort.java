2acbb08a6504cd7b322ce7c80db354dbe533a301
hengshaochen <henrychen0702@gmail.com>
2017-09-25 19:50:58 -0400

    9.25

--- Quick Sort.java
+++ Quick Sort.java
@@ -1,26 +1,31 @@
 public class Solution {
-    /**
-     * @param A an integer array
-     * @return void
+    /*
+     * @param A: an integer array
+     * @return: 
      */
     public void sortIntegers2(int[] A) {
+        // 兩個指標, left找比pivot大的, right找比pivot小的, 每回合swap完畢後, 左邊都會比pivot小, 右邊會比pivot大 --> 切割成子問題
         quickSort(A, 0, A.length - 1);
     }
     
-    private void quickSort(int[] A, int start, int end) {
+    public void quickSort(int[] A, int start, int end) {
         if (start >= end) { return; }
         int left = start;
         int right = end;
+        // 下面會出錯, 因為pivot有可能會被換掉, 之後指向他index的值就會變化
+        //int pivotIdx = (start + end) / 2;
         int pivot = A[(start + end) / 2];
+        
         while (left <= right) {
-            // left找比pivot大, right找比pivot小
             while (left <= right && A[left] < pivot) {
                 left++;
             }
+            
             while (left <= right && A[right] > pivot) {
                 right--;
             }
-            // 兩指針鎖定後進行swap
+            
+            // Swap
             if (left <= right) {
                 int temp = A[left];
                 A[left] = A[right];
@@ -29,8 +34,7 @@ public class Solution {
                 right--;
             }
         }
-        // 切割成子問題
-        quickSort(A, start, right);  // 左邊子問題(right剛剛向左移, 放right)
-        quickSort(A, left, end);     // 右邊子問題(left剛剛向右移, 放left)
-    } 
+        quickSort(A, start, right);
+        quickSort(A, left, end);
+    }
 }
