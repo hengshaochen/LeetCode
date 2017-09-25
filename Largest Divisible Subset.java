1fc3f3c25890a41d8ac7ffbe39dfef0cb3aa29d3
hengshaochen <henrychen0702@gmail.com>
2017-09-25 16:51:21 -0400

    9.25

---
+++ Largest Divisible Subset.java
@@ -0,0 +1,46 @@
+public class Solution {
+    /*
+    dp数组表示从0~i包括第i个元素最大的divisible subset size 
+    pre数组用来标记 状态转移过程中的方向，用于回溯最大值时的解集。 
+    dp[i]=max{dp[i],dp[j]+1}
+    */
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        
+        int[] f = new int[nums.length];
+        int[] pre = new int[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) { //初始化
+            f[i] = 1;
+            pre[i] = -1;
+        }
+        
+        // f[i]為最大的subset size
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                // 如果新來的數字都可以整除前面所有數字 --> 加入
+                if (f[i] < f[j] + 1 && nums[i] % nums[j] == 0) {
+                    f[i] = f[j] + 1;
+                    pre[i] = j;
+                }
+            }
+        }
+        
+        int maxIdx = -1, max = -1;
+        for (int i = 0; i < nums.length; i++) {//找到最大的子集size 和它最后元素的下标
+            if (f[i] > max) {
+                max = f[i];
+                maxIdx = i;
+            }
+        }
+        
+        for (int i = maxIdx; i >= 0;) { //回溯解集
+            ans.add(nums[i]);
+            i = pre[i];
+        }
+        
+        return ans;
+    }
+}
