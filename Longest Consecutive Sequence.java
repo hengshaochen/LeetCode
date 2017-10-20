91b482dc15eb10040134c68467f20a559699a7ac
hengshaochen <henrychen0702@gmail.com>
2017-10-20 16:13:32 -0400

    10.20

--- Longest Consecutive Sequence.java
+++ Longest Consecutive Sequence.java
@@ -1,3 +1,4 @@
+// O(n) HashSet
 public class Solution {
     /**
      * @param nums: A list of integers
@@ -26,6 +27,33 @@ public class Solution {
                 }
                 
                 ans = Math.max(ans, next - pre - 1);
+            }
+        }
+        return ans;
+    }
+}
+
+// O(nlogn) Sorting
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return an integer
+     */
+    public int longestConsecutive(int[] num) {
+        if (num.length == 0 || num == null) { return 0; }
+        Arrays.sort(num);
+        
+        int ans = 1;
+        int count = 1;
+        for (int i = 1; i < num.length; i++) {
+            if (num[i - 1] + 1 == num[i]) {
+                count++;
+                ans = Math.max(count, ans);
+            } else if (num[i - 1] == num[i]) {
+                // [0,1,1,2] 當num[2]時候數字為1, 這時不要歸零, 但也不要加count
+                continue;
+            } else {
+                count = 1;
             }
         }
         return ans;
