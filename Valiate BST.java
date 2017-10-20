070f55ce82613434771cf04dc13e2adf76499770
hengshaochen <henrychen0702@gmail.com>
2017-10-19 21:22:19 -0400

    10.19

---
+++ Valiate BST.java
@@ -0,0 +1,22 @@
+// version 3  Divide and Conquer
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        // write your code here
+        return divConq(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean divConq(TreeNode root, long min, long max){
+        if (root == null){
+            return true;
+        }
+        if (root.val <= min || root.val >= max){
+            return false;
+        }
+        return divConq(root.left, min, Math.min(max, root.val)) && 
+                divConq(root.right, Math.max(min, root.val), max);
+    }
+}
