e9a14052b55ae2385d93b3fbb2805c8776872744
hengshaochen <henrychen0702@gmail.com>
2017-10-27 01:23:16 -0400

    10.26

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -8,32 +8,37 @@
  *         this.next = null;
  *     }
  * }
- */ 
+ */
+
+
 public class Solution {
-    /**
-     * @param ListNode l1 is the head of the linked list
-     * @param ListNode l2 is the head of the linked list
+    /*
+     * @param l1: ListNode l1 is the head of the linked list
+     * @param l2: ListNode l2 is the head of the linked list
      * @return: ListNode head of linked list
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
+        ListNode tail = dummy;
         
-        while(l1 != null && l2 != null) {
-            if (l1.val > l2.val) {
-                cur.next = l2;
-                l2 = l2.next;
-            } else {
-                cur.next = l1;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
                 l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
             }
-            cur = cur.next;
+            tail = tail.next;
         }
+        
         if (l1 != null) {
-            cur.next = l1;
-        } else {
-            cur.next = l2;
+            tail.next = l1;
         }
+        if (l2 != null) {
+            tail.next = l2;
+        }
+        
         return dummy.next;
     }
 }
--- pow.java
+++ pow.java
@@ -30,3 +30,30 @@ public class Solution {
         return ans;
     }
 }
+
+// 好方法, 把次方用2進位來算
+public class Solution {
+    /*
+     * @param x: the base number
+     * @param n: the power number
+     * @return: the result
+     */
+    public double myPow(double x, long n) {
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        
+        double ans = 1;
+        double now_mul = x;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                ans *= now_mul;
+            }
+            now_mul *= now_mul;
+            n = n / 2;
+        }
+        
+        return ans;
+    }
+}
