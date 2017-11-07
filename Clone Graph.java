5551566da99f102418ce2e88322a2ed6c56f841d
hengshaochen <henrychen0702@gmail.com>
2017-11-07 02:20:55 -0500

    11.06

--- Clone Graph.java
+++ Clone Graph.java
@@ -140,3 +140,38 @@ public class Solution {
     }
 }
 */
+
+/*
+Review: 11/06 No ArrayList Space
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) { return null; }
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        Queue<UndirectedGraphNode> q = new LinkedList<>();
+        
+        q.add(node);
+        map.put(node, new UndirectedGraphNode(node.label));
+        
+        while (!q.isEmpty()) {
+            UndirectedGraphNode cur = q.remove();
+            
+            for (UndirectedGraphNode neighbor : cur.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    q.add(neighbor);
+                    map.put(neighbor, new UndirectedGraphNode(neighbor.label));
+                }
+                map.get(cur).neighbors.add(map.get(neighbor));
+            }
+        }
+        
+        return map.get(node);
+    }
+}
