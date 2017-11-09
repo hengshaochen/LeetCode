9745a7482d8d89e55c18b98b22ac142960623366
hengshaochen <henrychen0702@gmail.com>
2017-11-09 11:18:20 -0500

    11.09

--- Palindrome Linked List.java
+++ Palindrome Linked List.java
@@ -31,3 +31,72 @@ class Solution {
         return true;
     }
 }
+
+
+// Time: O(n) Space:O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/*
+ 1 -> 2 -> 3 -> N
+                 s         f
+                
+           1  -> 2 -> 3 -> 4 
+                 mid  
+*/
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        ListNode mid = findMid(head);
+        mid.next = revList(mid.next);
+        ListNode p1 = head;
+        ListNode p2 = mid.next;
+        
+        // odd: 1 -> 2  vs 1 -> 2 ok 
+        // even: 1 -> 2 -> 3 -> 4     p1: 1   ,  p2: 3 -> 4 (p2一定比p1長)
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        if (p2 != null) {
+            return false;
+        }
+        return true;
+        
+    }
+    
+    ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    ListNode revList(ListNode head) {
+        // 1 -> 2 -> N  --->  2 -> 1 -> N
+        ListNode pre = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+}
