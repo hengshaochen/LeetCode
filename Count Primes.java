fe6390d18d2ff37728f1a7bc22a75665fd53d1c6
hengshaochen <henrychen0702@gmail.com>
2017-11-12 15:44:43 -0500

    11.12

--- Count Primes.java
+++ Count Primes.java
@@ -3,9 +3,6 @@ class Solution {
     public int countPrimes(int n) {
         int ans = 0;
         for (int i = 2; i < n; i++) {
-            if (i > 2 && i % 2 == 0) {
-                continue;
-            }
             boolean tag = true;
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
